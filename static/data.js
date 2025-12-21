721a98ec9a2b3d6d3d7e2239619cfb578841ce7b
Tommy Hyland <71108113+Tommy-Hyland@users.noreply.github.com>
2025-12-20 21:41:32 -0600

    Add files via upload

---
+++ static/data.js
@@ -0,0 +1,41 @@
+// static/data.js
+// All images are WEBP
+// - Nightfarer portraits: ./static/portraits/<slug>.webp
+// - Expedition icons:     ./static/bosses/<Expedition Name>.webp
+
+window.__DATA__ = {
+  nightfarers: [
+    { name: "Wylder",      slug: "wylder",      imgUrl: "./static/portraits/wylder.webp" },
+    { name: "Guardian",    slug: "guardian",    imgUrl: "./static/portraits/guardian.webp" },
+    { name: "Duchess",     slug: "duchess",     imgUrl: "./static/portraits/duchess.webp" },
+    { name: "Revenant",    slug: "revenant",    imgUrl: "./static/portraits/revenant.webp" },
+    { name: "Ironeye",     slug: "ironeye",     imgUrl: "./static/portraits/ironeye.webp" },
+    { name: "Raider",      slug: "raider",      imgUrl: "./static/portraits/raider.webp" },
+    { name: "Recluse",     slug: "recluse",     imgUrl: "./static/portraits/recluse.webp" },
+    { name: "Executor",    slug: "executor",    imgUrl: "./static/portraits/executor.webp" },
+    { name: "Undertaker",  slug: "undertaker",  imgUrl: "./static/portraits/undertaker.webp" },
+    { name: "Scholar",     slug: "scholar",     imgUrl: "./static/portraits/scholar.webp" },
+  ],
+
+  bosses: [
+    { name: "Tricephalos",        slug: "tricephalos",         imgUrl: "./static/bosses/Tricephalos.webp" },
+    { name: "Gaping Jaw",         slug: "gaping-jaw",          imgUrl: "./static/bosses/Gaping Jaw.webp" },
+    { name: "Sentient Pest",      slug: "sentient-pest",       imgUrl: "./static/bosses/Sentient Pest.webp" },
+    { name: "Augur",              slug: "augur",               imgUrl: "./static/bosses/Augur.webp" },
+    { name: "Equilibrious Beast", slug: "equilibrious-beast",  imgUrl: "./static/bosses/Equilibrious Beast.webp" },
+    { name: "Darkdrift Knight",   slug: "darkdrift-knight",    imgUrl: "./static/bosses/Darkdrift Knight.webp" },
+    { name: "Fissure in the Fog", slug: "fissure-in-the-fog",  imgUrl: "./static/bosses/Fissure in the Fog.webp" },
+    { name: "Night Aspect",       slug: "night-aspect",        imgUrl: "./static/bosses/Night Aspect.webp" },
+    { name: "Balancers",          slug: "balancers",           imgUrl: "./static/bosses/Balancers.webp" },
+    { name: "Dreglord",           slug: "dreglord",            imgUrl: "./static/bosses/Dreglord.webp" },
+  ],
+
+  earth: [
+    { name: "None",                         slug: "none" },
+    { name: "The Crater",                   slug: "the-crater" },
+    { name: "Mountaintop",                  slug: "mountaintop" },
+    { name: "Rotted Woods",                 slug: "rotted-woods" },
+    { name: "Noklateo, the Shrouded City",  slug: "noklateo-the-shrouded-city" },
+    { name: "The Great Hollow",             slug: "the-great-hollow" },
+  ],
+};
